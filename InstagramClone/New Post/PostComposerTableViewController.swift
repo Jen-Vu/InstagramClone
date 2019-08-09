648abda4f4d957bb1084266fb24a9cedb4905d5b
Juan Dorado <fdorado985@gmail.com>
2019-08-08 21:47:35 -0500

    Create Post Composer UI

---
+++ InstagramClone/New Post/PostComposerTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PostComposerTableViewController.swift
+//  InstagramClone
+//
+//  Created by Juan Francisco Dorado Torres on 8/8/19.
+//  Copyright © 2019 Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import UIKit
+
+class PostComposerTableViewController: UITableViewController {
+
+  // MARK: - View Lifecycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
